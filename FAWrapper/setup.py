a9461ddef62e32b327aae8ecbcd56773fd56150a
BuildTools <unconfigured@null.spigotmc.org>
2020-06-12 12:32:27 -0700

    Initial commits

---
+++ FAWrapper/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FAWrapper-huantian",
+    version="0.1.0",
+    author="David Li",
+    author_email="davidtianli@gmail.com",
+    description="A python wrapper for faexport",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/huantianad/FAWrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+    python_requires='>=2.7'
+)
